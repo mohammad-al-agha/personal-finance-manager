927e22bab94c78133b1b5b913caf83cb4dd8924f
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-02 15:07:04 +0300

    Backend: Created the expense schema

---
+++ backend/src/expense/expenseModel.ts
@@ -0,0 +1,27 @@
+import { HydratedDocument } from 'mongoose';
+import { Schema, Prop } from '@nestjs/mongoose';
+
+export type ExpenseDocument = HydratedDocument<Expense>;
+
+export enum ExpenseCategories {
+  FURNITURE = 'Furniture',
+  GROCERY = 'Grocery',
+  HEALTH = 'Health',
+  MAINTENANCE = 'Maintenance',
+  RENT = 'Rent',
+}
+
+@Schema()
+export class Expense {
+  @Prop({ required: true })
+  amount: number;
+
+  @Prop({ required: true })
+  date: Date;
+
+  @Prop({ required: true })
+  user: string;
+
+  @Prop()
+  category: ExpenseCategories;
+}
