ddefe225bf38e4abff3f114113aebbdac1280d58
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-05 15:29:50 +0300

    Downloaded the config package

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ExpenseModule } from './expense/expense.module';
 import { IncomeModule } from './income/income.module';
+import { ConfigModule } from '@nestjs/config';
 require('dotenv').config();
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.DB_URL),
     ExpenseModule,
     IncomeModule,
