5626356dcc12bae9f2631a492a77ac744be44e16
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-05 13:54:52 +0300

    Addded the create income API

--- backend/src/income/income.service.ts
+++ backend/src/income/income.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Income } from './incomeModel';
+import mongoose from 'mongoose';
+import { createIncomeDTO } from './dto';
 
 @Injectable()
-export class IncomeService {}
+export class IncomeService {
+  constructor(
+    @InjectModel(Income.name)
+    private incomeModel = mongoose.Model<Income>,
+  ) {}
+
+  async createIncome(createIncomeDto: createIncomeDTO): Promise<Income> {
+    const { title, amount, date, user, category } = createIncomeDto;
+    const income = new this.incomeModel({
+      title,
+      amount,
+      date,
+      user,
+      category,
+    });
+    return income.save();
+  }
+}
