07ae3ddab11524ae0b3a11d82027c28200c0cbe4
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-05 13:30:06 +0300

    Added the income module to the app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ExpenseModule } from './expense/expense.module';
+import { IncomeModule } from './income/income.module';
 require('dotenv').config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URL), ExpenseModule],
+  imports: [MongooseModule.forRoot(process.env.DB_URL), ExpenseModule, IncomeModule],
 })
 export class AppModule {}
