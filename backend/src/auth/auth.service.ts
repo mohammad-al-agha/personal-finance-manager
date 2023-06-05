7e3b3a990d69060d8ce2c5c014727f61e231bc83
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-06 00:40:52 +0300

    Injected the user model and the jwt service in the auth service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/users/userModel';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private readonly jwtService = JwtService,
+  ) {}
+}
