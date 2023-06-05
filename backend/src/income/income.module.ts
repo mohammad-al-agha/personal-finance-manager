6ff4a41869109169c62b291a9edf7e3362c5f7c6
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-05 14:08:09 +0300

    Imported the Income model in the app module

--- backend/src/income/income.module.ts
+++ backend/src/income/income.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { IncomeController } from './income.controller';
 import { IncomeService } from './income.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Income, IncomeSchema } from './incomeModel';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Income.name, schema: IncomeSchema }]),
+  ],
   controllers: [IncomeController],
-  providers: [IncomeService]
+  providers: [IncomeService],
 })
 export class IncomeModule {}
