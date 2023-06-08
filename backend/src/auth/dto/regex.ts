b63fa85db170619095228f52f5330f70adcac11a
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-08 13:06:18 +0300

    Created the regex functions to validate the email and password

---
+++ backend/src/auth/dto/regex.ts
@@ -0,0 +1,8 @@
+export function EmailValidation(email: string): boolean {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+export function PasswordValidation(password: string): boolean {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  return passwordRegex.test(password);
+}
