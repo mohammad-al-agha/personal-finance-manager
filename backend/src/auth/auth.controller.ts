965a6b4cc3d4686ec885a3a2f02f6a8d45c7d795
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-08 16:55:48 +0300

    Added the login API to the auth controller under a post http request

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { signUpDTO } from './dto/signUpDTO';
+import { loginDTO } from './dto/loginDTO';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,12 @@ export class AuthController {
   @Post('register')
   async signUp(@Body() data: signUpDTO): Promise<object> {
     const response = await this.authService.signUP(data);
+    return response;
+  }
+
+  @Post('login')
+  async login(@Body() data: loginDTO): Promise<object> {
+    const response = await this.authService.login(data);
     return response;
   }
 }
