082aac37c560fe956127c4403dbf7c5388f1a4bd
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-08 11:08:14 +0300

    injected the user model in the user service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './users.model';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+}
