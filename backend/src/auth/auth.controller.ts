851c28cd3c5a90b5e90f4a97b89c82c459c071df
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-08 13:07:56 +0300

    Added the auth service sign up API to the auth controller with a POST request

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { signUpDTO } from './dto/signUpDTO';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async signUp(@Body() data: signUpDTO): Promise<object> {
+    const response = await this.authService.signUP(data);
+    return response;
+  }
+}
