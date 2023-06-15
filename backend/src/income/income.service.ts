dca962788bdbf0af4e2d3ce8fcc697a44abdaa03
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-15 15:56:11 +0300

    Passed the user Id to the user attribute in the add income API

--- backend/src/income/income.service.ts
+++ backend/src/income/income.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Income } from './incomeModel';
-import mongoose from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 import { createIncomeDTO } from './dto';
+import { User, UserDocument } from 'src/users/users.model';
 
 @Injectable()
 export class IncomeService {
   constructor(
     @InjectModel(Income.name)
     private incomeModel = mongoose.Model<Income>,
+    @InjectModel(User.name)
+    private userModel = mongoose.Model<UserDocument>,
   ) {}
 
-  async addIncome(createIncomeDto: createIncomeDTO): Promise<Income> {
+  async addIncome(
+    createIncomeDto: createIncomeDTO,
+    userId: Types.ObjectId,
+  ): Promise<Income> {
+    const id = userId;
+
     //Extracting the props from the DTO
-    const { title, amount, date, user, category } = createIncomeDto;
+    const { title, amount, date, category } = createIncomeDto;
 
     //creating a new income
     const income = new this.incomeModel({
       title,
       amount,
       date,
-      user,
+      user: id,
       category,
     });
     return income.save();
