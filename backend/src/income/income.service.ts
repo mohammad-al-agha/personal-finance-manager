9cca54b6873210119f16861b2e502682f8731f19
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-24 21:19:05 +0300

    Added the get incomes API in the income service

--- backend/src/income/income.service.ts
+++ backend/src/income/income.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Req } from '@nestjs/common';
+import { HttpException, Injectable, Req } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Income } from './incomeModel';
 import mongoose, { Types } from 'mongoose';
@@ -9,7 +9,9 @@ import { User, UserDocument } from '../users/users.model';
 export class IncomeService {
   constructor(
     @InjectModel(Income.name)
-    private incomeModel = mongoose.Model<Income>,
+    private incomeModel: mongoose.Model<Income>,
+    @InjectModel(User.name)
+    private userModel: mongoose.Model<User>,
   ) {}
 
   async addIncome(
@@ -17,6 +19,9 @@ export class IncomeService {
     userId: Types.ObjectId,
   ): Promise<Income> {
     const id = userId;
+
+    //getting teh user by id
+    const user = await this.userModel.findById(userId);
 
     //Extracting the props from the DTO
     const { title, amount, date, category } = createIncomeDto;
@@ -29,6 +34,25 @@ export class IncomeService {
       user: id,
       category,
     });
+
+    //adding the new income to the list of expenses that this user has
+    user.incomes.push(income);
+
+    //saving the updated user
+    await user.save();
+
     return income.save();
+  }
+
+  async getIncomes(userId: Types.ObjectId) {
+    //getting the user by id
+    const user = await this.userModel.findById(userId);
+
+    //checking if the user exists
+    if (!user) {
+      throw new HttpException('User not found', 404);
+    }
+
+    return user.incomes;
   }
 }
