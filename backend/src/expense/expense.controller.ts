b698c2f94a448f11ff5a342b018bc1ebf3d5a649
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-24 18:53:55 +0300

    Added the get expenses method in the expense controller

--- backend/src/expense/expense.controller.ts
+++ backend/src/expense/expense.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Get } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
-import { createExpenseDTO } from './dto';
+import { createExpenseDTO } from './dto/createExpense';
 import { AuthGuard } from '../auth/auth.guards';
 
 @Controller('expenses')
@@ -13,11 +13,24 @@ export class ExpenseController {
     @Body() createExpenseDTO: createExpenseDTO,
     @Req() req: Request,
   ) {
+    //extracting the user id from the request
     const userId = req['userId'];
+
     const expense = await this.expenseService.addExpense(
       createExpenseDTO,
       userId,
     );
+
     return { expense };
+  }
+
+  @Get()
+  async getExpenses(@Req() req: Request) {
+    //extracting the user id from the request
+    const userId = req['userId'];
+
+    const expenses = await this.expenseService.getExpenses(userId);
+
+    return { expenses };
   }
 }
