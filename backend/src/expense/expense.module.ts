846978aabc8f3d9980a96a47dbdcf123eec577d3
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-24 18:53:15 +0300

    Added the user model to the expense module

--- backend/src/expense/expense.module.ts
+++ backend/src/expense/expense.module.ts
@@ -3,10 +3,12 @@ import { ExpenseController } from './expense.controller';
 import { ExpenseService } from './expense.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Expense, ExpenseSchema } from './expenseModel';
+import { User, UserSchema } from '../users/users.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Expense.name, schema: ExpenseSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [ExpenseController],
   providers: [ExpenseService],
