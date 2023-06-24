c0fe6a23c75f44caba696ea709bf536e3b3d5118
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-24 18:56:03 +0300

    Added the user model to the income module

--- backend/src/income/income.module.ts
+++ backend/src/income/income.module.ts
@@ -3,10 +3,12 @@ import { IncomeController } from './income.controller';
 import { IncomeService } from './income.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Income, IncomeSchema } from './incomeModel';
+import { User, UserSchema } from '../users/users.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Income.name, schema: IncomeSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [IncomeController],
   providers: [IncomeService],
