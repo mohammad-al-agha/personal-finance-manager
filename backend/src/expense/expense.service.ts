5db36b5c3edcc4dd3d3e43683efb0079b9bdb31e
MohammadAlAgha <mohalagha11@gmail.com>
2023-06-24 18:54:53 +0300

    Added the get expenses API method and saved the user after adding new expense

--- backend/src/expense/expense.service.ts
+++ backend/src/expense/expense.service.ts
@@ -1,14 +1,17 @@
-import { Injectable, Post } from '@nestjs/common';
+import { HttpException, Injectable, Post } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Expense, ExpenseCategories } from './expenseModel';
 import mongoose, { Types } from 'mongoose';
-import { createExpenseDTO } from './dto';
+import { createExpenseDTO } from './dto/createExpense';
+import { User } from '../users/users.model';
 
 @Injectable()
 export class ExpenseService {
   constructor(
     @InjectModel(Expense.name)
     private expenseModel: mongoose.Model<Expense>,
+    @InjectModel(User.name)
+    private userModel: mongoose.Model<User>,
   ) {}
 
   async addExpense(
@@ -16,6 +19,9 @@ export class ExpenseService {
     userId: Types.ObjectId,
   ): Promise<Expense> {
     const id = userId;
+
+    //getting hthe user by id
+    const user = await this.userModel.findById(id);
 
     //Extracting the props from the DTO
     const { title, amount, date, category } = createExpenseDto;
@@ -28,6 +34,25 @@ export class ExpenseService {
       user: id,
       category,
     });
+
+    //adding the new espense to the list of expenses that this user has
+    user.expenses.push(expense);
+
+    //saving the updated user
+    await user.save();
+
     return expense.save();
+  }
+
+  async getExpenses(userId: Types.ObjectId) {
+    //getting the user id from the request
+    const user = await this.userModel.findById(userId);
+
+    //checking if the user exists
+    if (!user) {
+      throw new HttpException('User not found', 400);
+    }
+
+    return user.expenses;
   }
 }
