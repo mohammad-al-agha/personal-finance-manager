4a8b7045abb59621568777e7d0a8fdbbe1b338cd
MohammadAlAgha <mohalagha11@gmail.com>
2023-07-08 00:13:14 +0300

    created the edit income API in the income service

--- backend/src/income/income.service.ts
+++ backend/src/income/income.service.ts
@@ -1,9 +1,10 @@
 import { HttpException, Injectable, Req } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Income } from './incomeModel';
-import mongoose, { Types } from 'mongoose';
+import mongoose, { Promise, Types } from 'mongoose';
 import { createIncomeDTO } from './dto/createIncome';
 import { User, UserDocument } from '../users/users.model';
+import { updateIncomeDTO } from './dto/updateIncomeDTO';
 
 @Injectable()
 export class IncomeService {
@@ -44,7 +45,7 @@ export class IncomeService {
     return income.save();
   }
 
-  async getIncomes(userId: Types.ObjectId) {
+  async getIncomes(userId: Types.ObjectId): Promise<Income[]> {
     //getting the user by id
     const user = await this.userModel.findById(userId);
 
@@ -56,7 +57,10 @@ export class IncomeService {
     return user.incomes;
   }
 
-  async deleteIncome(userId: Types.ObjectId, incomeId: Types.ObjectId) {
+  async deleteIncome(
+    userId: Types.ObjectId,
+    incomeId: Types.ObjectId,
+  ): Promise<Income[]> {
     //getting the user by id
     const user = await this.userModel.findById(userId);
 
@@ -91,6 +95,52 @@ export class IncomeService {
       user.save(),
       this.incomeModel.findByIdAndDelete(incomeId),
     ]);
+
+    return user.incomes;
+  }
+
+  async editIncome(
+    userId: Types.ObjectId,
+    incomeId: Types.ObjectId,
+    updateIncomeDTO: updateIncomeDTO,
+  ): Promise<Income[]> {
+    //getting the user by id
+    const user = await this.userModel.findById(userId).populate('incomes');
+
+    //checking if the user exists
+    if (!user) {
+      throw new HttpException('User not found', 404);
+    }
+
+    //getting the income by id
+    let income = await this.incomeModel.findById(incomeId);
+
+    //checking if the income exists
+    if (!income) {
+      throw new HttpException('No such income', 404);
+    }
+
+    //searching for the income in the array of incomes of the user
+    const incomeIndex = user.incomes.findIndex(
+      (income) => income['_id'].toString() === incomeId.toString(),
+    );
+    //then checking if it exists
+    if (incomeIndex === -1) {
+      throw new HttpException('No such income for this user', 404);
+    }
+
+    //updating the values after insuring that this income is for this user
+    income.title = updateIncomeDTO.title;
+    income.amount = updateIncomeDTO.amount;
+    income.date = updateIncomeDTO.date;
+    income.category = updateIncomeDTO.category;
+
+    //updating the income for the user
+    user.incomes[incomeIndex] = income;
+
+    //saving all changes
+    await user.save();
+    await income.save();
 
     return user.incomes;
   }
